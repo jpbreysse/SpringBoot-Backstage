3af91bed07f7471e16de46f22621933e743cf69c
Ashe <89166015+AsheOne18@users.noreply.github.com>
2021-12-02 23:14:52 -0500

    Add files via upload

---
+++ src/test/java/com/ashen/userlogin/Userlogin02ApplicationTests.java
@@ -0,0 +1,36 @@
+package com.ashen.userlogin;
+
+import com.ashen.userlogin.bean.MainMenu;
+//import com.naughty.userlogin02.dao.MenuDao;
+import com.ashen.userlogin.dao.MenuDao;
+import com.ashen.userlogin.dao.UserDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class Userlogin02ApplicationTests {
+
+    @Autowired
+    UserDao userDao;
+    @Autowired
+    MenuDao menuDao;
+
+    @Test
+    void contextLoads() {
+//        List<User> ulist = userDao.getAllUser(null,0,2);
+//        System.out.println("总人数为: "+ulist.size());
+//        for (User us:ulist
+//             ) {
+//            System.out.println(us);
+//        }
+
+        List<MainMenu> mainMenus = menuDao.getMainMenus();
+        for (MainMenu mm:mainMenus) {
+            System.out.println(mm);
+        }
+    }
+
+}
